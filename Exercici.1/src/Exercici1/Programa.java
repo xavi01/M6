b32f4e17f668674947b9e9801ce69ea5661abc16
xavi01 <xavier.navarro.mas@gmail.com>
2020-09-17 10:42:15 +0200

    hola

--- Exercici.1/src/Exercici1/Programa.java
+++ Exercici.1/src/Exercici1/Programa.java
@@ -5,21 +5,48 @@ public class Programa {
 	public static void main(String[]args) {
 		
 	Scanner scan= new Scanner(System.in); 
-	System.out.print("Entrar numero 1: "); 
-	int n1 = scan.nextInt();
-	System.out.print("Entra numero 2: "); 
-	int n2 = scan.nextInt(); 
-	int n3 = n1+n2; 
-	System.out.println("La súma és: " + n3); 
-	if(n3>0 && n3<=5) { 
-		System.out.println("1"); 
-	} else if(n3>5 && n3<=8) {
-		System.out.println("2"); 
-		} 
-	else if(n3>8) {
-		System.out.println("3"); } 
-	else { 
-		System.out.println("ERROR!"); 
+	int a=0;
+	
+	System.out.print("Entrar numero de alumnes: "); 
+
+	a= scan.nextInt();
+	scan.nextLine();
+	
+	String [] nomAlumnes = new String [a];
+	
+	int c=0;
+	
+	while(c<a) {
+		
+		System.out.println("Entrar el nom de l'alumne " + c + ": "); 
+		nomAlumnes[c] = scan.nextLine();
+		c++;
+		
+	}
+	
+	for(int i=0; i<nomAlumnes.length; i++) {		
+		System.out.println(nomAlumnes[i]); 
+	}
+	
+	
+	
+	
+	
+	
+	//System.out.print("Entra numero 2: "); 
+	//int n2 = scan.nextInt(); 
+	//int n3 = n1+n2; 
+	//System.out.println("La súma és: " + n3); 
+	//if(n3>0 && n3<=5) { 
+	//System.out.println("1"); 
+	//} else if(n3>5 && n3<=8) {
+	//	System.out.println("2"); 
+	//	} 
+	//else if(n3>8) {
+	//	System.out.println("3"); } 
+	//else { 
+	//	System.out.println("ERROR!"); 
 	}
 } 
-	}
+	
+ 
--- Exercici1/src/Exercici1/programa.java
+++
@@ -1,9 +0,0 @@
-package Exercici1;
-
-public class programa {
-
-	public static void main(String[]args) {
-		System.out.println("Hola mon");
-	}
-	
-}
